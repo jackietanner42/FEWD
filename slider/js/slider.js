898862bcbd969f253dab701183b3ed9663f9ba0e
Jackie Tanner <jaqtann@gmail.com>
2015-06-17 21:37:16 -0700

    Correct Slider/Comparisons

--- slider/js/slider.js
+++ slider/js/slider.js
@@ -4,42 +4,62 @@ Element.prototype.slider = function(){
   var slider = this;
   var wrapper = slider.children[0];
   var slides = wrapper.children;
-  var position = 1;
+  var position = 0;
   var width = window.innerWidth;
   var leftButton = document.createElement('div');
   var rightButton = document.createElement('div');
 
 
-  var init = function() {
-
-    wrapper.style.width = slides.length * width + 'px';
-    wrapper.style.height = '100%';
+  this.createButtons = function(){
+    console.log(leftButton);
 
     leftButton.classList.add('left');
     rightButton.classList.add('right');
 
-    slider.appendChild(leftButton);
-    slider.appendChild(rightButton);
-
-    for(var index=0; index<slides.length; index++){
-       slides[index].style.width = width + 'px';
-    }
 
     leftButton.addEventListener('mousedown',function(){
-        wrapper.style.marginLeft = width * position * -1 + 'px';
-        position = position + 1;
+
+      if(position > (width * (slides.length - 1)) * -1) {
+        position = position - width;
+        wrapper.style.marginLeft = position + 'px';
+      }
+
     });
 
     rightButton.addEventListener('mousedown',function(){
-        position = position - 1;
-        wrapper.style.marginLeft = width * position * -1 + 'px';
+
+      if(position < 0) {
+        position = position + width;
+        wrapper.style.marginLeft = position + 'px';
+      }
     });
 
+      slider.appendChild(leftButton);
+      slider.appendChild(rightButton);
+    };
+
+  this.resize = function() {
+
+    width = window.innerWidth;
+
+    wrapper.style.width = slides.length * width + 'px';
+    wrapper.style.height = '100%';
+    for(var index=0; index<slides.length; index++){
+       slides[index].style.width = width + 'px';
 
   };
 
-  init();
+
+  this.init = function() {
+
+    this.createButtons();
+    this.resize();
+    window.addEventListener('resize',slider.resize);
+  };
+
+  this.init();
 
 
 };
 /* end Slider */
+};
