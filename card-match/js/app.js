945a55e8f8d2ab8e9e07e06ba2941e0a863a9c63
Jackie Tanner <jaqtann@gmail.com>
2015-07-08 21:33:13 -0700

    Card Match Flip

---
+++ card-match/js/app.js
@@ -0,0 +1,16 @@
+// navElement.showSection('back');
+
+(function() {
+ var cardflip = document.querySelectorAll(".card.effect__click");
+ for ( var i  = 0, len = cardflip.length; i < len; i++ ) {
+   var card = cardflip[i];
+   clickListener( card );
+ }
+
+ function clickListener(card) {
+   card.addEventListener( "click", function() {
+     var c = this.classList;
+     c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+   });
+ }
+})();
