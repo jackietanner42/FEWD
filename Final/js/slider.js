a829b125428e9e14094f5d17d0a49b0e1d995cb3
Jackie Tanner <jaqtann@gmail.com>
2015-07-29 13:02:45 -0700

    Final, dot dot dots

--- Final/js/slider.js
+++ Final/js/slider.js
@@ -5,27 +5,31 @@ Element.prototype.Slider = function(){
   var wrapper = slider.children[0];
   var slides = wrapper.children;
   var position = 0;
+  var index = 0;
   var width = window.innerWidth;
   var leftButton = document.createElement('div');
   var rightButton = document.createElement('div');
 
 
-  this.createButtons = function(){
+  var createButtons = function(){
 
     leftButton.classList.add('left');
     rightButton.classList.add('right');
 
-
     leftButton.addEventListener('mousedown',function(){
-      if(position > (width * (slides.length - 1)) * -1) {
-        position = position - width;
+      if(position < 0) {
+        index--;
+        updateCurrentPage(index);
+        position = position + width;
         wrapper.style.marginLeft = position + 'px';
       }
     });
 
     rightButton.addEventListener('mousedown',function(){
-      if(position < 0) {
-        position = position + width;
+      if(position > (width * (slides.length - 1)) * -1) {
+        index++;
+        updateCurrentPage(index);
+        position = position - width;
         wrapper.style.marginLeft = position + 'px';
       }
     });
@@ -34,7 +38,33 @@ Element.prototype.Slider = function(){
       slider.appendChild(rightButton);
     };
 
-  this.resize = function() {
+  var createPages = function(){
+    var pages = document.getElementById('pages');
+
+    for(var i = 0; i < slides.length; i++){
+      var page = document.createElement('a')
+      page.id = i;
+      page.addEventListener('mousedown',function(){
+        // goToPage(i);
+      });
+
+      page.className = 'page off';
+      pages.appendChild(page);
+    }
+
+    pages.children[0].className = 'page on';
+  };
+
+  var updateCurrentPage = function(index){
+    var pages = document.getElementById('pages');
+    for(var i = 0; i < pages.children.length; i++){
+      var page = pages.children[i];
+      page.className = 'page off';
+    }
+    pages.children[index].className = 'page on';
+  };
+
+  var resize = function() {
 
     width = window.innerWidth;
 
@@ -45,7 +75,7 @@ Element.prototype.Slider = function(){
 
        slides[index].children[0].style.width = width + 'px';
        slides[index].style.width = width + "px";
-       slides[index].style.height = slides[index].children[0].clientHeight + "px";
+       slides[index].style.height = "500px"; //slides[index].children[0].clientHeight + "px";
 
        if(slides[index].children[0].clinetHeight <= slides[0].children[0].clientHeight || index===0){
          wrapper.style.height = slider.style.height = slides[index].children[0].clientHeight + "px";
@@ -57,15 +87,19 @@ Element.prototype.Slider = function(){
   };
 
 
-  this.init = function(){
+  var init = function(){
 
-    slider.resize();
-    slider.createButtons();
+    resize();
+    createButtons();
+    createPages();
+
+    // find out how many images there are on the page
+    var pages = document.getElementById('pages');
 
     window.addEventListener('resize',slider.resize);
   };
 
-  this.init();
+  init();
 
 
 };
